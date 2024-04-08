8960017759b11e3d754589e26eadaccb779d321c
Daniel Cuesta <116822538+DanielCuesta98@users.noreply.github.com>
2024-04-08 20:42:44 +0200

    ProyectoReact
    Nuevo proyecto con react y express

---
+++ proyectoreact/app.js
@@ -0,0 +1,21 @@
+// app.js
+
+const express = require('express');
+const path = require('path');
+const employeeRoutes = require('./routes/employeeRoutes');
+const cors = require('cors');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/src')));
+
+app.use(cors());
+app.use('/api', employeeRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/src', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+});
